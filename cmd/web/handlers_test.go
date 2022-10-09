22384ffdf2d75122541148b408e8a6121d322c99
pippimotta <lingyizh84@gmail.com>
2022-10-09 20:44:36 +0900

    add e2e test for usersignup

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/pippimotta/snippet-box/internal/assert"
@@ -69,4 +70,122 @@ func TestSnippetView(t *testing.T){
 			}
 		})
 	}
+}
+
+
+func TestUserSignup(t *testing.T){
+	app := newTestApplication(t)
+	ts := newTestServer(t,app.routes())
+
+	defer ts.Close()
+
+	//Make a GET/user/signup request and then extract the CSRF token from the response body
+	_,_, body := ts.get(t,"/user/signup")
+	validCSRFToken := extractCSRFToken(t, body)
+
+	const (
+		validName = "Bob"
+		validPassword = "validPas$$word"
+		validEmail= "bob@example.com"
+		formTag = "<form action='/user/signup' method='POST' novalidate>"
+	)
+
+	tests := []struct{
+		name string
+		userName string
+		userEmail string
+		userPassword string
+		csrfToken string
+		wantCode int
+		wantFormTag string
+	}{
+		{
+			name:"Valid submission",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusSeeOther,
+		},
+		{
+			name: "Invalid CSRF",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: validPassword,
+			csrfToken: "wrongToken",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name: "Empty Name",
+			userName: "",
+			userEmail: validEmail,
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,
+		},
+		{
+			name: "Empty Email",
+			userName: validName,
+			userEmail: "",
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,	
+		},
+		{
+			name: "Empty Password",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: "",
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,	
+		},
+		{
+			name: "Invalid Email",
+			userName: validName,
+			userEmail: "bob@example.",
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,	
+		},
+		{
+			name: "Short Password",
+			userName: validName,
+			userEmail: validEmail,
+			userPassword: "pa$$",
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,	
+		},
+		{
+			name: "Duplicate Email",
+			userName: validName,
+			userEmail: "dupe@example.com",
+			userPassword: validPassword,
+			csrfToken: validCSRFToken,
+			wantCode: http.StatusUnprocessableEntity,
+			wantFormTag: formTag,	
+		},
+	}
+
+	for _,tt := range tests{
+		t.Run(tt.name,func(t *testing.T){
+			form := url.Values{}
+			form.Add("name",tt.userName)
+			form.Add("email",tt.userEmail)
+			form.Add("password",tt.userPassword)
+			form.Add("csrf_token",tt.csrfToken)
+
+			code, _, body := ts.postForm(t,"/user/signup",form)
+			assert.Equal(t,code, tt.wantCode)
+			if tt.wantFormTag != ""{
+				assert.StringContains(t, body,tt.wantFormTag)
+			}
+		})
+	}
+
+
 }
--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"html"
 	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 
@@ -73,6 +76,34 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	if err != nil {
 		t.Fatal(err)
 	}
+	bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+//Define a regular expression which catches the CSRF token from the HTML 
+var csrfTokenRX = regexp.MustCompile(`<input type="hidden" name="csrf_token" value="(.+)">`)
+
+func extractCSRFToken(t *testing.T, body string) string{
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+	t.Fatal("no csrf token found in body") }
+	return html.UnescapeString(string(matches[1]))
+}
+
+//Create a postForm method for sending POST request to the test server
+func (ts *testServer)postForm(t *testing.T, urlPath string, form url.Values)(int, http.Header, string){
+	rs, err := ts.Client().PostForm(ts.URL+urlPath,form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	bytes.TrimSpace(body)
 
 	return rs.StatusCode, rs.Header, string(body)
